3ff8eac92a049af1d2f103267d69ea629e00bf97
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-08-19 15:47:26 -0700

    Add Get method

--- AnimalShelter/Controllers/AnimalShelterController.cs
+++ AnimalShelter/Controllers/AnimalShelterController.cs
@@ -5,35 +5,25 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-//namespace AnimalShelter.Controllers
-//{
-//  [ApiController]
-//  [Route("[controller]")]
-//  public class AnimalShelterController : ControllerBase
-//  {
-//    private static readonly string[] Summaries = new[]
-//    {
-//      "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//    }
-//
-//    private readonly ILogger<AnimalShelterController> _logger;
-//
-//    public AnimalShelterController(ILogger<AnimalShelterController> logger)
-//    {
-//      _logger = logger;
-//    }
-//
-//    [HttpGet]
-//    public IEnumerable<AnimalShelter> Get()
-//    {
-//      var rng = new Random();
-//      return Enumerable.Range(1, 5).Select(index => new AnimalShelter
-//      {
-//        Date = DateTime.Now.AddDays(index),
-//        TemperatureC = rng.Next(-20, 55),
-//        Summary = Summaries[rng.Next(Summaries.Length)]
-//      })
-//      .ToArray();
-//    }
-//  }
-//}
+namespace AnimalShelter.Controllers
+{
+  [ApiController]
+  [Route("api/[controller]")]
+  public class AnimalsController : ControllerBase
+  {
+    private readonly AnimalShelterContext _db;
+
+    public AnimalsController(AnimalShelterContext db)
+    {
+      _db = db;
+    }
+  
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Animal>>> Get()
+
+    {
+      return await _db.Animals.ToListAsync();
+    }
+  }
+}

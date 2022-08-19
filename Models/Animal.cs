40a8121a4e780214134e314ed889ab659555bde7
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-08-19 14:42:16 -0700

    Create Animal.cs

---
+++ Models/Animal.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace AnimalShelter.Models
+{
+  public class WeatherForecast
+  {
+    public string Species { get; set; }
+    public string Name { get; set; }
+    public DateTime Birthday { get; set; }
+    public int Age { get; set; }
+    public string Gender { get; set; }
+  }
+}

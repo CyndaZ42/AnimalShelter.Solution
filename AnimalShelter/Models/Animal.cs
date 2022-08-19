185b6d76172622053754c21b70a43c82e5efe1cc
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-08-19 15:39:00 -0700

    Add Initial migration

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Globalization;
+using System.ComponentModel.DataAnnotations;
 
 namespace AnimalShelter.Models
 {
@@ -6,6 +9,7 @@ namespace AnimalShelter.Models
     public int AnimalId { get; set; }
     public string Species { get; set; }
     public string Name { get; set; }
+    public DateTime Birthday { get; set; }
     public int Age { get; set; }
     public string Gender { get; set; }
   }

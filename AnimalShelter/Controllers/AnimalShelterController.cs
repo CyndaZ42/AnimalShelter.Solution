7d487b40a60759eec0df4225fe9aa34cd0117297
Zachary Waggoner <zacwaggoner42@gmail.com>
2022-08-28 13:53:26 -0700

    Add Put route

--- AnimalShelter/Controllers/AnimalShelterController.cs
+++ AnimalShelter/Controllers/AnimalShelterController.cs
@@ -39,6 +39,35 @@ namespace AnimalShelter.Controllers
       return animal;
     }
 
+    [HttpPut("{id}")]
+    public async Task<IActionResult> Put(int id, Animal animal)
+    {
+      if (id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+
+      _db.Entry(animal).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+
+      return NoContent();
+    }
+
     [HttpPost]
     public async Task<ActionResult<Animal>> Post(Animal animal)
     {
@@ -46,6 +75,11 @@ namespace AnimalShelter.Controllers
       await _db.SaveChangesAsync();
 
       return CreatedAtAction(nameof(GetAnimal), new { id = animal.AnimalId }, animal);
+    }
+
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
     }
   }
 }
